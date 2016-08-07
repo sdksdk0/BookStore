f22f91360deff454b9682c44f36ad204adfab341
sdksdk0 <690644511@qq.com>
2016-08-07 21:11:24 +0800

    delete  password

--- src/cn/tf/utils/SendMailThread.java
+++ src/cn/tf/utils/SendMailThread.java
@@ -42,7 +42,7 @@ public class SendMailThread extends Thread{
 			message.saveChanges();
 			
 			Transport ts = session.getTransport();
-			ts.connect("xingtian@tianfang1314.cn", "13579012aA");
+			ts.connect("你的邮箱账号", "你的密码");
 			ts.sendMessage(message, message.getAllRecipients());
 			ts.close();
 		} catch (AddressException e) {
